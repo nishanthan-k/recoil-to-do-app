69aecf3d8017e865c57c0f99280839fde66b1a14
Nishanthan K <krn.nishanthan@gmail.com>
2024-07-06 17:09:47 +0530

    fix: added hover effects and text general css to  todo list

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ToDoList from "./components/ToDoList"
 
 function App() {
   return (
-    <main className="w-screen h-screen bg-slate-700 flex justify-center p-10">
+    <main className="w-screen h-screen bg-slate-800 flex justify-center py-10 px-10 mb:px-7">
       <section className="max-w-xl w-full flex flex-col gap-10">
         <AddNewToDo />
         <ToDoList />
--- src/components/ToDoCard.jsx
+++ src/components/ToDoCard.jsx
@@ -5,16 +5,16 @@ function ToDoCard(props) {
   const {todo: {task, isCompleted}} = props;
   
   return (
-    <div className={`grid grid-flow-col place-content-between place-items-center py-4 px-2 border-2 border-purple-700  ${isCompleted && 'line-through'}`}>
+    <div className={`bg-slate-700 text-gray-300 grid grid-flow-col place-content-between place-items-center py-4 px-2 border-2 border-purple-700 rounded-md ${isCompleted && 'line-through text-gray-500'}`}>
       {isCompleted ? 
-        <MdOutlineCheckCircleOutline className="cursor-pointer" /> 
+        <MdOutlineCheckCircleOutline className="cursor-pointer checkCircleHoverEffect" /> 
         :
-        <MdOutlineRadioButtonUnchecked className="cursor-pointer mdtext-xl" />
+        <MdOutlineRadioButtonUnchecked className="cursor-pointer mdtext-xl checkCircleHoverEffect" />
       }
-      <p className="overflow-auto mb:max-w-52 sm:max-w-96">
+      <p className="overflow-auto mb:max-w-52 sm:max-w-96 ">
         {task}
       </p>
-      <MdDelete className="text-xl cursor-pointer transform transition-transform duration-200 hover:scale-125 hover:text-red-500" />
+      <MdDelete className={`text-xl ${!isCompleted ? 'cursor-pointer trashHoverEffect hover:text-red-500' : 'cursor-default'}`} />
     </div>
   )
 }
